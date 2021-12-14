fc2bf02309d89a1d98574eacc4b44071cb57c4c1
DevJH <hkonly@korea.ac.kr>
2021-12-14 22:09:11 +0900

    udp_linux

---
+++ udp_linux_receiver.cpp
@@ -0,0 +1,42 @@
+#include <netinet/in.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char** argv) {
+    int sock;
+    sock = socket(PF_INET, SOCK_DGRAM, 0);
+    if (sock == -1) {
+        printf("Failed to create a socket!\n"); 
+        return -1;
+    }
+    printf("Succeeded to create a socket.\n");
+
+    struct sockaddr_in server_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family	= PF_INET; // IPv4
+    server_addr.sin_port	= htons(4000); // Port number
+    server_addr.sin_addr.s_addr	= htonl(INADDR_ANY); // 32bit IP address (or inet_addr("0.0.0.0"))
+
+    if (bind(sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
+        printf("Failed to bind socket!\n");
+        return -1;
+    }
+    printf("Succeeded to bind socket.\n");
+
+    // declare client struct to get client information
+    struct sockaddr_in client_addr;
+    unsigned int client_addr_size = sizeof(client_addr);
+
+    // receive message
+    while(true) {
+        const int BUFF_SIZE = 10;
+        char buff_rcv[BUFF_SIZE];
+        recvfrom(sock, buff_rcv, BUFF_SIZE, 0, (struct sockaddr*)&client_addr, &client_addr_size); 
+
+		printf("Buffer:");
+		for (int i=0 ; i<BUFF_SIZE ; i++) { printf(" %x"); }
+		printf("\n");
+    }
+
+    return 0;
+}
---
+++ udp_linux_sender.cpp
@@ -0,0 +1,27 @@
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char** argv) {
+    int sock = socket(PF_INET, SOCK_DGRAM, 0);
+    if (sock == -1) {
+        printf("Failed to create a socket!\n");
+        return -1;
+    }
+
+    struct sockaddr_in server_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(4000);
+    server_addr.sin_addr.s_addr = inet_addr("127.30.1.60");
+
+    while(true) {
+        char buff[10] = {0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a};
+        sendto(sock, buff, sizeof(buff), 0, (struct sockaddr*)&server_addr, sizeof(server_addr)); 
+        usleep(500);
+    }
+
+    return 0;
+}
