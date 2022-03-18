c73bf7c9490f18b2a42ec8b7acd12a3a105c4270
DevJH <hkonly@korea.ac.kr>
2022-03-18 09:23:34 +0900

    update array_int_char.cpp

--- array_int_char.cpp
+++ array_int_char.cpp
@@ -4,6 +4,13 @@ int main() {
     int a[] = {100, 200};
     unsigned char* c = (unsigned char*)&a;
 
+    printf("Size of int array that has 2 elements: %ld\n", sizeof(a));
+    printf("Byte stream of a\t:");
+    for (int i=0; i<sizeof(a); i++) {
+        printf(" %02x", *((char*)a+i)); 
+    }
+    printf("\n");
+
     printf("Value of a in hex\t:");
     for (int i=0 ; i<(sizeof(a)/sizeof(a[0])) ; i++) {
         printf(" %08x", a[i]);
